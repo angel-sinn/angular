9c9238baa708bc515287b828a09291334784fade
Angel <angelkysinn@gmail.com>
2021-06-08 16:15:23 -0700

    Tracking and handling input state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  passwordLength = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
   password = '';
 
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+    console.log(value);
+
+    if (!isNaN(parsedValue)) {
+      this.passwordLength = parsedValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
   onButtonClick() {
+    console.log(`
+      About to generate a passsword with the following:
+      Includes letter: ${this.includeLetters}
+      Includes number: ${this.includeNumbers}
+      Includes symbol: ${this.includeSymbols}
+    `);
     this.password = 'My Password!';
   }
 }
