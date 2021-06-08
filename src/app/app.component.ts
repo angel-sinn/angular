7019e1155b7484a5148354c7368617c5dab7305c
Angel <angelkysinn@gmail.com>
2021-06-08 15:21:31 -0700

    Event and property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'password';
+  password = '';
+
+  onButtonClick() {
+    this.password = 'My Password!';
+  }
 }
